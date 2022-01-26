394c036379ed10aba33eba5714281ad9d13e07ca
Florian <florian@sixtl.at>
2022-01-26 19:51:58 +0100

    Initial commit

---
+++ src/main/java/at/fsix/mdown/MDownApplication.java
@@ -0,0 +1,13 @@
+package at.fsix.mdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MDownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fsix/mdown/MDownApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fsix.mdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MDownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
